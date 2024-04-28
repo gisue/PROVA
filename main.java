bb479ebd7df29eedc691610690395c744cd0dcfd
gisue <giuseppe.maugeri@itiszuccante.edu.it>
2024-04-28 20:19:02 +0200

    modified:   main.java

--- main.java
+++ main.java
@@ -1,5 +1,8 @@
 public class main {
     public static void main(String[] args) {
         System.out.println("ciao");
+        if(3>2){
+            System.out.println("sèèèè");
+        }
     }
 }
