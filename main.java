12b4d84cf93301788222ced68f06e4a59501a421
peppow <155969124+gisue@users.noreply.github.com>
2024-04-28 20:30:07 +0200

    Delete main.java

--- main.java
+++
@@ -1,8 +0,0 @@
-public class main {
-    public static void main(String[] args) {
-        System.out.println("ciao");
-        if(3>2){
-            System.out.println("sèèèè");
-        }
-    }
-}
